9b94b68be3e76a6f9e6834dff3be9520bc961021
unknown <fajardillah25@gmail.com>
2024-04-16 06:28:37 +0700

    Update

--- src/test/java/belajar/java/stream/StreamBuilder.java
+++ src/test/java/belajar/java/stream/StreamBuilder.java
@@ -1,2 +1,36 @@
-package belajar.java.stream;public class StreamBuilder {
+package belajar.java.stream;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Objects;
+import java.util.stream.Stream;
+
+public class StreamBuilder {
+    @Test
+    void testCreateStreamBuilder() {
+
+        Stream.Builder<String> builder = Stream.builder();
+
+
+        //pada cara pertama konsepnya seperti chaining, kita bisa terus menerus menambahkan secara manual
+        builder.add("fajar").add("abdillah");//ini cara pertama
+        builder.accept("ahmad");//ini cara kedua
+
+        Stream<String> stream = builder.build();
+        stream.forEach(data-> System.out.println(data));
+
+    }
+
+
+    @Test
+    void testCreateStreamBuilderSimple() {
+
+        //INI CARA YANG LEBIH SIMPLE dengan cara memakai typegenerate Object
+        // pada akhir jangan lupa method .build()
+
+        Stream<Object> simpleBuilder = Stream.builder().add("membaca").add("buku").add("baru").build();//ini hanya bisa 1baris saja
+        simpleBuilder.forEach(System.out::println);
+
+
+    }
 }
--- src/test/java/belajar/java/stream/StreamPipeline.java
+++ src/test/java/belajar/java/stream/StreamPipeline.java
@@ -1,2 +1,26 @@
-package belajar.java.stream;public class StreamPipeline {
+package belajar.java.stream;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+public class StreamPipeline {
+
+
+    @Test
+    void testCreateStreamPipeline() {
+        //TODO: Programmer java biasanya menggunakan cara karna lebih mudah dibaca, adappun cara yang ada di file StreamOperator itu sangat jarang sekali digunakan
+
+        List<String> list = List.of("nur asiah","maisaroh","inne qonitati");
+
+        list.stream()//langkah awal kita harus menggunakan method stream()
+                .map(upper -> upper.toUpperCase())//ini adalah modifikasi yang ingin kita tambahkan ke dalam var list
+                .map(addNy -> "Ny. "+addNy)
+                .forEach(System.out::println);//jika sudah untuk menjalankan Streamnya kita gunakan forEach()
+
+
+
+
+
+    }
 }
