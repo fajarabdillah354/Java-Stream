720a8b705fc92c783f02dca0a9f818510d491a0e
unknown <fajardillah25@gmail.com>
2024-04-16 22:39:12 +0700

    Update

--- src/test/java/belajar/java/stream/CreateStreamTest.java
+++ src/test/java/belajar/java/stream/CreateStreamTest.java
@@ -5,7 +5,6 @@ import org.junit.jupiter.api.Test;
 import java.awt.desktop.SystemEventListener;
 import java.util.Arrays;
 import java.util.Collection;
-import java.util.Collections;
 import java.util.List;
 import java.util.stream.Stream;
 
