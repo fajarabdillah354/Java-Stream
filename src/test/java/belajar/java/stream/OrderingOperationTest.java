cffeeecaefb58a8f8f6f325eb89bf09726fffb60
unknown <fajardillah25@gmail.com>
2024-05-07 18:28:52 +0700

    Update

--- src/test/java/belajar/java/stream/OrderingOperationTest.java
+++ src/test/java/belajar/java/stream/OrderingOperationTest.java
@@ -1,2 +1,27 @@
-package belajar.java.stream;public class OrderingOperationTest {
+package belajar.java.stream;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Comparator;
+import java.util.List;
+
+public class OrderingOperationTest {
+
+    @Test
+    void testSorted(){
+        List.of("fajar","abdillah","ahmad","keren").stream()
+                .sorted()
+                .forEach(System.out::println);
+    }
+
+
+    @Test
+    void testSortedComparator(){
+        Comparator<String> comparator = Comparator.reverseOrder();
+
+        List.of("fajar","abdillah","ahmad","keren").stream()
+                .sorted(comparator)
+                .forEach(System.out::println);
+    }
+
 }
